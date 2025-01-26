2af73d3f04ed75c8eab6fcb7a4445f8d93ffcb76
springboot20 <abbasopeyemi148@gmail.com>
2025-01-26 18:15:07 +0100

    updated heror section and deleted data folder

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -3,7 +3,7 @@ export function HeroSection() {
     <main className="bg-hero bg-no-repeat h-screen relative bg-cover lg:bg-bottom  before:content-[''] before:inset-0 before:bg-black/55 before:h-full before:absolute">
       <div className='px-4 lg:px-0 max-w-6xl mx-auto text-center z-10 relative h-full grid place-items-center'>
         <div className="space-y-1">
-          <h1 className='font-bold font-nunito text-2xl sm:text-3xl lg:text-5xl text-white leading-loose'>
+          <h1 className='font-bold font-nunito text-2xl sm:text-3xl lg:text-5xl text-white lg:leading-loose'>
             Stay Ahead Of The Curve: Stay Informed With Our Blog For The Latest Industry Insights
           </h1>
           <p className='text-lg font-nunito-sans text-white font-semibold'>
